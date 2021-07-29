9f4b70f77b265bea358cf6091a9876f28778d3fb
@konstantin_videnin <videnin2012@yandex.ru>
2021-07-29 19:40:34 +0300

    three sections

---
+++ js/linza.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.header__top .header__search-btn').
+  mouseover(function(){
+    $('.header__top .header__search-text').toggleClass('open');
+  });
+
+
+
+
+
+  });
---
+++ js/skroll.js
@@ -0,0 +1,13 @@
+
+  const choi = document.querySelectorAll('.choices__list[role="listbox"]');
+  choi.forEach(element => {  //одуреть как все сложно!! два дня убил
+    element.setAttribute('data-simplebar','null');
+  });
+
+
+
+
+
+
+
+
